7ee07749e248b1e84e1286664d256c48d5faf749
Kiet Nguyen <kietdev38@gmail.com>
2016-03-03 09:34:42 +0700

    update problem question

--- problem_6_sum_square_difference.rb
+++ problem_6_sum_square_difference.rb
@@ -1,3 +1,13 @@
+# The sum of the squares of the first ten natural numbers is,
+#
+# 12 + 22 + ... + 102 = 385
+# The square of the sum of the first ten natural numbers is,
+#
+# (1 + 2 + ... + 10)2 = 552 = 3025
+# Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+#
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+#
 sum1 = (1..100).inject(0) {|sum, i| sum += i}
 sum2 = (1..100).inject(0) {|sum, i| sum += i * i}
 puts sum1 * sum1 - sum2
--- problem_7_10001st_prime.rb
+++ problem_7_10001st_prime.rb
@@ -1,7 +1,6 @@
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+# What is the 10 001st prime number?
 #
-# find the number in list of primes which has position is 10001st
-#
-
 def is_prime_number?(number)
   return false if number < 2
   return !2.upto(Math.sqrt(number) + 1).detect {|e| number % e == 0}
